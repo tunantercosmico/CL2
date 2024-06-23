7cb1cf87791680e48f1be7e56bb227d9b221fcfb
tunantercosmico <carlos.hilario.trinidad@gmail.com>
2024-06-23 01:30:01 -0500

    segundo

--- src/main/java/controlador/ControladorUsuario.java
+++ src/main/java/controlador/ControladorUsuario.java
@@ -2,13 +2,18 @@ package controlador;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ClassUsuarioImp;
+import model.TblUsuariocl2;
+
 /**
  * Servlet implementation class ControladorUsuario
  */
+@WebServlet("/login")
 public class ControladorUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,7 +30,8 @@ public class ControladorUsuario extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("FormLogin.jsp").forward(request, response);
+		
 	}
 
 	/**
@@ -33,7 +39,18 @@ public class ControladorUsuario extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		ClassUsuarioImp crud = new ClassUsuarioImp();
+
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+
+        TblUsuariocl2 user = crud.validarUsuario(usuario, password);
+        if(user !=null){
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }else{
+            request.setAttribute("error", "Usuario o contraseña incorrectos");
+            request.getRequestDispatcher("FormLogin.jsp").forward(request, response);
+        }
 	}
 
 }
--- src/main/java/dao/ClassUsuarioImp.java
+++ src/main/java/dao/ClassUsuarioImp.java
@@ -1,5 +1,35 @@
 package dao;
 
-public class ClassUsuarioImp {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import interfaces.IUsuario;
+import model.TblUsuariocl2;
+
+public class ClassUsuarioImp implements IUsuario{
+
+	@Override
+	public TblUsuariocl2 validarUsuario(String usuario, String password) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("LPII_CL2_HILARIOTRINIDADCARLOS");
+        EntityManager em = emf.createEntityManager();
+        try{
+            TypedQuery<TblUsuariocl2> query = em.createQuery(
+                    "SELECT u FROM TblUsuariocl2 u WHERE u.usuariocl2 = :usuario AND u.passwordcl2 = :password",TblUsuariocl2.class);
+            query.setParameter("usuario", usuario);
+            query.setParameter("password", password);
+
+            return query.getSingleResult();
+        }//fin del try
+        catch(NoResultException e){
+            return null;
+        }//fin del catch
+        finally{
+            em.close();
+        }
+	}
 
 }
